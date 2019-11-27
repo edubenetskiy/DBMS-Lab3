52113b0e5f15fbba350050674609d401da68eb99
Egor Dubenetskiy <egor@banka.space>
2019-11-28 02:33:48 +0300

    Create backup directory at target host on upload

--- backup/upload.sh
+++ backup/upload.sh
@@ -4,4 +4,5 @@ workspace=`dirname $0`/../
 . $workspace/env.sh
 
 : ${TARGET_HOST:=oracle@db184}
+ssh $TARGET_HOST "mkdir -p $BACKUP_DIR"
 rsync --rsync-path=/opt/csw/bin/rsync --archive --progress --verbose $BACKUP_DIR $TARGET_HOST:$BACKUP_DIR
